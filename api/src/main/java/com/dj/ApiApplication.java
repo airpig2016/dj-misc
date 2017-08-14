80e45945a143ca7aad6b41705903e7f5176f6b42
chen dajie <zhanzh@91asking.com>
2017-08-14 09:45:54 +0800

    修改api pom，解决其他工程无法应用api工程接口问题

--- api/src/main/java/com/dj/ApiApplication.java
+++ api/src/main/java/com/dj/ApiApplication.java
@@ -3,7 +3,7 @@ package com.dj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
